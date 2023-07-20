e1550949173e93904568e8abc217efe92dd2071e
Jubibani <125711731+Jubibani@users.noreply.github.com>
2023-07-19 22:29:24 -0700

    updates

---
+++ home.js
@@ -0,0 +1,10 @@
+// This is my first javascript code!
+console.log('hellow');
+// function showMessage(element) {
+//     element.setAttribute('data-message', 'This will lead to another page');
+//   }
+  
+//   function hideMessage(element) {
+//     element.removeAttribute('data-message');
+//   }
+  
